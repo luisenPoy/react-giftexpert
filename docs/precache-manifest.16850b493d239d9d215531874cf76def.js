94a97f249553a464307cc76332bc1c036b93a66d
Luisen Poy <kttweb@gmail.com>
2020-10-19 19:47:40 +0200

    agregado docs

---
+++ docs/precache-manifest.16850b493d239d9d215531874cf76def.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b86935d629ce951132650c6d45237a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b155a0f595da77850167",
+    "url": "/static/css/main.19ba2687.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b155a0f595da77850167",
+    "url": "/static/js/main.377c3cda.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
